918a7b1476ddfeb80fa0393399e7c9b89623cae9
Tanishq Bhatia <tanishqbhatia474@gmail.com>
2025-02-04 10:47:53 +0530

    add stock option added

--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Child1Component } from '../child1/child1.component';
 import { Child2Component } from '../child2/child2.component';
 import { Child3Component } from '../child3/child3.component';
+import { Child4Component } from '../child4/child4.component';
 
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [CommonModule, Child1Component, Child2Component, Child3Component],
+  imports: [CommonModule, Child1Component, Child2Component, Child3Component,Child4Component],
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
